65b008335c5ebf886218ad4b84bfbd9c025e40bb
G <g@devnull.rip>
2026-09-14 03:55:49 -0700

    secure file downloads

--- internal/uploads/handler.go
+++ internal/uploads/handler.go
@@ -87,7 +87,7 @@ func (handler *Handler) Upload(responseWriter http.ResponseWriter, request *http
 }
 
 func (handler *Handler) Download(responseWriter http.ResponseWriter, request *http.Request) {
-	_, ok := handler.requireAuth(responseWriter, request)
+	session, ok := handler.requireAuth(responseWriter, request)
 	if !ok {
 		return
 	}
@@ -101,7 +101,7 @@ func (handler *Handler) Download(responseWriter http.ResponseWriter, request *ht
 		handler.internalError(responseWriter, request, err)
 		return
 	}
-	if !found {
+	if !found || (file.UserID != session.Session.UserID && session.User.Role != "support" && session.User.Role != "admin") {
 		handler.fileNotFound(responseWriter)
 		return
 	}
